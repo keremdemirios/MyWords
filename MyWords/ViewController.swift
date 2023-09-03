61e582c65a2a079e7a7266d7dce9fe021698bcdc
Kerem Demir <keremdemir1694@gmail.com>
2023-09-03 17:12:38 +0300

    Programmatic design ready.

---
+++ MyWords/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  MyWords
+//
+//  Created by Kerem Demir on 3.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK : UI Elements
+    
+    // MARK : Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        configure()
+    }
+    // MARK : Configure
+    private func configure(){
+        setupUI()
+    }
+    // MARK : Setup UI
+    private func setupUI(){
+        
+    }
+    // MARK : Functions
+    
+    // MARK : Actions
+
+}
+
