b7ec6ed74a85b21ba0c9282df8334c42b216b604
Kerem Demir <keremdemir1694@gmail.com>
2023-09-03 19:15:53 +0300

    Second set localized

---
+++ MyWords/String+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  String+Extensions.swift
+//  MyWords
+//
+//  Created by Kerem Demir on 3.09.2023.
+//
+
+import Foundation
+import UIKit
+
+//MARK: - Localizable
+func localized() -> String {
+    return NSLocalizedString(
+        self,
+        tableName: "Localizable",
+        bundle: .main,
+        value: self,
+        comment: self
+    )
+}
